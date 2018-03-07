644a3070c9757b653f0374e83f77f07ca07b413a
Joan Miquel Torres <joanmi@bitifet.net>
2018-03-07 14:38:09 +0100

    Started worker handling externalization.
    Extracted worker creation (and removal) outside of generated function.
    
    Non production-ready version.
    
    This is only first step to more efficient approach where there could be
    more than one workers that could also be shared among multiple
    functions.

--- index.js
+++ index.js
@@ -8,67 +8,83 @@
 var Worker = require('webworker-threads').Worker;
 var Deasync = require('deasync');
 
+
+function createWorker() {//{{{
+    return new Worker(function(){
+        var fn;
+        this.onmessage = function(event) {
+            switch (event.data.oper) {
+                case "src":
+                    // "Compile" function thougt source evaluation.
+                    try {
+                        eval ("fn = " + event.data.msg + ";");
+                        postMessage(['ready']);
+                    } catch (e) {
+                        postMessage(['error', "Error trying to evaluate function source"]);
+                    };
+                    break;
+                case "args":
+                    // Call the function with given arguments and send its result back.
+                    try {
+                        postMessage(["return", fn.apply({}, event.data.msg)]);
+                    } catch (e) {
+                        postMessage(['error', e]);
+                    };
+                    break;
+                case "end":
+                    // Gracefully close worker.
+                    self.close();
+                    break;
+            };
+        };
+    });
+};//}}}
+
+
+
 function funwork(fn){
 
     var source = fn.toString();
 
+    // Create worker:
+    var worker = createWorker(); // FIXME (intermediate change)
+
+    // Send function source to worker:
+    worker.postMessage({oper: "src", msg: source});
+
+    var ready = false;
+    var done;
+    var args;
+    var error, retv;
+
+    // Event handling://{{{
+    worker.onmessage = function(event) {
+        switch (event.data[0]) {
+            case 'error':
+                ///worker.postMessage({oper: "end"});
+                done = true;
+                error = event.data[1];
+                break;
+            case 'ready':
+                ready = true;
+                break;
+            case 'return':
+                ///worker.postMessage({oper: "end"});
+                done = true;
+                retv = event.data[1];
+                break;
+        };
+    };//}}}
+
+
     return function() {
 
-        var done = false;
-        var args = Array.prototype.slice.call(arguments);
-        var error, retv;
+        done = false;
+        args = Array.prototype.slice.call(arguments);
 
-        // Create worker://{{{
-        var worker = new Worker(function(){
-            var fn;
-            this.onmessage = function(event) {
-                switch (event.data.oper) {
-                    case "src":
-                        // "Compile" function thougt source evaluation.
-                        try {
-                            eval ("fn = " + event.data.msg + ";");
-                            postMessage(['ready']);
-                        } catch (e) {
-                            postMessage(['error', "Error trying to evaluate function source"]);
-                        };
-                        break;
-                    case "args":
-                        // Call the function with given arguments and send its result back.
-                        try {
-                            postMessage(["return", fn.apply({}, event.data.msg)]);
-                        } catch (e) {
-                            postMessage(['error', e]);
-                        };
-                        break;
-                    case "end":
-                        // Gracefully close worker.
-                        self.close();
-                        break;
-                };
-            };
-        });//}}}
-        
-        // Event handling://{{{
-        worker.onmessage = function(event) {
-            switch (event.data[0]) {
-                case 'error':
-                    worker.postMessage({oper: "end"});
-                    done = true;
-                    error = event.data[1];
-                    break;
-                case 'ready':
-                    worker.postMessage({oper: "args", msg: args});
-                    break;
-                case 'return':
-                    worker.postMessage({oper: "end"});
-                    done = true;
-                    retv = event.data[1];
-                    break;
-            };
-        };//}}}
-
-        // Send function source to worker:
-        worker.postMessage({oper: "src", msg: source});
+        // Wait (without blocking) until ready:
+        Deasync.loopWhile(function(){return !ready;});
+        worker.postMessage({oper: "args", msg: args});
 
         // Wait (without blocking) until done:
         Deasync.loopWhile(function(){return !done;});
